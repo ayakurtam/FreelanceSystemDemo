da4854b812c5200c3ebce2e13ec6f88e10ac48ff
ayakurtam <aya.mohamed200077@yahoo.com>
2021-05-30 18:51:47 +0200

    New 1

--- FreelanceSystemDemo/Models/AccountViewModels.cs
+++ FreelanceSystemDemo/Models/AccountViewModels.cs
@@ -65,6 +65,10 @@ namespace WebApplication1.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
